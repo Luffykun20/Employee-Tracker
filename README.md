16909999f9d84337677233a837b65584a28065ac
cristhian bustamante <cristhian_georgia_20@hotmail.com>
2023-03-17 13:24:27 -0400

    bugs fixed

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # Employee-Tracker
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)  
+  
+    
+## [Table of Contents](#table-of-contents)
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Test](#test)
+- [License](#license)
+- [Questions](#questions)
+
+## [Description](#table-of-contents)
+
+This Command-line application tracks and manages a company's employee database information using Node.js, Inquirer, and MYSQL technologies. 
+
+## [Installation](#table-of-contents)
+    
+To install this  Command-line application:
+
+* Clone the repository's code
+* Open it in VS Code, choose and  open the terminal of your preference
+* Install the required packages (Inquirer, MYSQL2, console.table)
+
+## [Usage](#table-of-contents)
+    
+To use this  Command-line application, enter the command "node index.js" in your terminal, follow and answer the prompts.
+
+The following video link demostrates the steps for using this application: [DemostrationVideo](https://drive.google.com/file/d/1R2_cjcYuZGVCXOzEj4FOE4uwt-9no-ve/view)??
+    
+
+  ## [Contributing](#table-of-contents)
+
+  Thanks! But I am not accepting foreign contributions at the moment.
+
+## [Test](#table-of-contents)
+
+N/A
+
+## [License](#table-of-contents)
+
+
+    Copyright (C) 2023 Cristhian Bustamante
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.       
+   
+   https://opensource.org/licenses/MIT
+        
+## [Questions](#table-of-contents)
+
+- You can reach me through https://github.com/Luffykun20
+- Also, you can contact me through my e-mail: [cristhian_georgia_20@hotmail.com](mailto:cristhian_georgia_20@hotmail.com) if you have any question.
--- index.js
+++ index.js
@@ -1,26 +1,205 @@
+// required packages
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table')
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: 'Mugiwar@2020!',
-      database: 'deparments_info'
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'Mugiwar@2020!',
+        database: 'departments_info'
     },
     console.log(`Connected to the deparments_info database.`)
-  );
+);
+db.connect(() => {
+    init()
+})
 
-
-
+// function that starts the application and shows the prompts menu
 function init() {
     inquirer.prompt([
         {
             type: "list",
             name: "options",
             message: "What would you like to request?",
-            choices: ["View all deparments", "View all roles", ""]
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add department",
+                "Add role",
+                "Add employee",
+                "Update employee role"]
+
         }
+    ]).then(responds => {
+        if (responds.options === "View all departments") {
+            viewDepartments()
+        } else if (responds.options === "View all roles") {
+            viewRoles()
+        } else if (responds.options === "View all employees") {
+            viewEmployees()
+        } else if (responds.options === "Add department") {
+            addDepartment()
+        } else if (responds.options === "Add role") {
+            addRole()
+        } else if (responds.options === "Add employee") {
+            addEmployee()
+        } else if (responds.options === "Update employee role") {
+            updateEmployeeRole()
+        }
+    })
+}
+// shows all departments
+function viewDepartments() {
+    db.query("SELECT * FROM departments", (err, data) => {
+        console.table(data)
+        init()
+    })
+}
+
+//shows all roles
+function viewRoles() {
+    db.query("SELECT roles.id, roles.title, departments.name AS department, roles.salary FROM roles INNER JOIN departments ON roles.department_id = departments.id", (err, data) => {
+        console.table(data)
+        init()
+    })
+}
+
+//shows all employees
+function viewEmployees() {
+    db.query(`SELECT employees.id, employees.first_name, employees.last_name, roles.title,
+    departments.name AS department, roles.salary,
+    CONCAT (manager.first_name, " ", manager.last_name)
+    AS manager FROM employees
+    LEFT JOIN roles ON employees.role_id = roles.id
+    LEFT JOIN departments ON roles.department_id = departments.id
+    LEFT JOIN employees manager ON employees.manager_id = manager.id`, (err, data) => {
+        console.table(data)
+        init()
+    })
+}
+
+// add new department into database
+function addDepartment() {
+    inquirer.prompt([{
+        type: "input",
+        name: "addDepartment",
+        message: "What is the new department would you like to add?",
+    }])
+        .then(responds => {
+            db.query("INSERT INTO departments(name) VALUES(?)", [responds.addDepartment], (err) => {
+                console.log(`New department has been added sucessfully!`)
+                init()
+            })
+        })
+}
+
+//add new role to a selected department into database
+function addRole() {
+    db.query("SELECT name ,id value FROM departments",(err, data) => {
+        
+    
+    inquirer.prompt([{
+        type: "input",
+        name: "addRole",
+        message: "What is the new role would you like to add?",
+    },
+    {
+        type: "input",
+        name: "salary",
+        message: "What is the salary for the new role?"
+    },
+    {
+        type: "list",
+        name: "departmentId",
+        message: "Which department does the new role belongs to?",
+        choices: data
+    }
     ])
+        .then(responds => {
+
+            db.query(
+                `INSERT INTO roles(title, salary, department_id) VALUES(?, ?, ${parseInt(responds.departmentId)})`,
+                [responds.addRole, responds.salary, responds.departmentId], (err) => {
+                    console.log(`New role has been added sucesfully`)
+                    init()
+                })
+        })
+    })
+}
+
+//add new employee into the database
+function addEmployee() {
+    db.query("SELECT title name, id value FROM roles", (err, data) => {
+        db.query("SELECT CONCAT(first_name,' ', last_name) name, id value FROM employees", (err, newData) => {
+
+       
+        
+    
+    inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: "What is the new employee's first name?"
+    },
+    {
+        type: "input",
+        name: "lastName",
+        message: "What is the new employee's last name?"
+    },
+    {
+        type: "list",
+        name: "roleId",
+        message: "What is the new employee's role?",
+        choices: data
+    },
+    {
+        type: "list",
+        name: "managerId",
+        message: "Who is the new employee's manager?",
+        choices: newData
+    }])
+        .then(responds => {
+            db.query(
+                `INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES(?, ?, ${parseInt(responds.roleId)}, ${parseInt(responds.managerId)})`,
+                [responds.name, responds.lastName, responds.roleId, responds.managerId], (err) => {
+                    console.log(`New employee has been added sucesfully`)
+                    init()
+                })
+        })
+    })
+    })
+    }
+
+// update employee's role into datase
+function updateEmployeeRole() {
+    db.query("SELECT CONCAT(first_name,' ', last_name) name , id value FROM employees", (err, data) => {
+        db.query("SELECT title name, id value FROM roles ", (err, roleData) => {
+
+        
+    
+    inquirer.prompt([{
+        type: "list",
+        name: "employeeUpdate",
+        message: "Which employee's role would you like to update?",
+        choices: data
+    },
+    {
+        type: "list",
+        name: "newRole",
+        message: "Which new role would you like to assign for the selected employee?",
+        choices: roleData
+    }])
+        .then(responds => {
+            db.query(
+                "UPDATE employees SET role_id =? WHERE id=?",
+                [responds.newRole, responds.employeeUpdate], (error) => {
+                    init()
+                }
+            )
+        })
+    }) 
+})
 }
